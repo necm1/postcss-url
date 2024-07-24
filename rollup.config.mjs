7566e578689035810a2cd732a21e1eb5ddbfa418
necm1 <necmi.m.hasan@outlook.de>
2024-07-24 03:09:58 +0200

    feat(develop): initialize repository, added package.json, added eslint & prettier, added rollup, added husky, added .nvmrc

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [nodeResolve(), commonjs(), typescript(), terser()],
+};
