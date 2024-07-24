c244922aa1d57fb8b9300e837cec330fad75f1ed
necm1 <necmi.m.hasan@outlook.de>
2024-07-24 21:19:19 +0200

    fix: changed behavior of building the files and folders

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,21 +3,62 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
+import { readdirSync, statSync } from 'fs';
+import { join } from 'path';
+
+const getFiles = (dir) => {
+  const files = readdirSync(dir);
+  let fileList = [];
+  files.forEach((file) => {
+    const filePath = join(dir, file);
+    if (statSync(filePath).isDirectory()) {
+      fileList = fileList.concat(getFiles(filePath));
+    } else {
+      fileList.push(filePath);
+    }
+  });
+  return fileList;
+};
+
+const processorFiles = getFiles('src/processor').filter((file) =>
+  file.endsWith('.ts'),
+);
+const utilFiles = getFiles('src/util').filter((file) => file.endsWith('.ts'));
+
 export default {
-  input: 'src/index.ts',
+  input: {
+    index: 'src/index.ts',
+    processor: 'src/processor.ts',
+    ...processorFiles.reduce((inputs, file) => {
+      const relativePath = file.replace(/^src\//, '').replace(/\.ts$/, '');
+      inputs[relativePath] = file;
+      return inputs;
+    }, {}),
+    ...utilFiles.reduce((inputs, file) => {
+      const relativePath = file.replace(/^src\//, '').replace(/\.ts$/, '');
+      inputs[relativePath] = file;
+      return inputs;
+    }, {}),
+  },
   output: [
     {
-      file: 'dist/index.js',
+      dir: 'dist',
       format: 'cjs',
-      sourcemap: true,
       exports: 'named',
+      entryFileNames: '[name].js',
     },
     {
-      file: 'dist/index.esm.js',
+      dir: 'dist',
       format: 'esm',
-      sourcemap: true,
-      exports: 'named',
+      entryFileNames: '[name].esm.js',
     },
   ],
-  plugins: [nodeResolve(), commonjs(), typescript(), terser()],
+  plugins: [
+    nodeResolve({
+      extensions: ['.ts', '.js'],
+    }),
+    commonjs(),
+    typescript(),
+    terser(),
+  ],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Processor } from './processor';
 import { ProcessorOptions } from './interface/processor.interface';
-import path from 'path';
-import { AbstractProcessor } from './processor/abstract-processor';
+import { AbstractProcessor } from './processor/abstract.processor';
 
 /**
  * PostCSS URL Plugin
@@ -32,41 +31,47 @@ import { AbstractProcessor } from './processor/abstract-processor';
  *
  * @returns {Function} A PostCSS plugin function.
  */
-const urlPlugin = (options: ProcessorOptions) => {
-  const processor = new Processor(options);
-
+const urlPlugin = (options: ProcessorOptions | ProcessorOptions[]) => {
   return {
     postcssPlugin: 'postcss-url',
     Once: async (root, { result }) => {
-      const filePaths: string[] = [];
+      const processorsList = Array.isArray(options) ? options : [options];
 
-      root.walkDecls((decl: { value: string }) => {
-        const urlMatch = decl.value.match(/url\(['"]?([^'")]+)['"]?\)/);
-        if (urlMatch) {
-          const urlPath = urlMatch[1];
-          filePaths.push(urlPath);
-        }
-      });
+      for (const processorOptions of processorsList) {
+        const processorType =
+          typeof processorOptions.url === 'function'
+            ? 'custom'
+            : processorOptions.url;
 
-      try {
-        const copiedFiles = await processor.process(filePaths);
-        copiedFiles.forEach((filePath, index) => {
-          const urlPath = filePaths[index];
-          root.walkDecls((decl: { value: string; source: any }) => {
-            if (decl.value.includes(urlPath)) {
-              const relativePath = path.relative(
-                path.dirname(decl.source.input.file),
-                filePath,
-              );
-              decl.value = decl.value.replace(
-                urlPath,
-                relativePath.split(path.sep).join('/'),
-              );
-            }
-          });
+        const processor = new Processor({
+          ...processorOptions,
+          url: processorType,
         });
-      } catch (error) {
-        result.warn(`Error processing files: ${error.message}`);
+
+        const filePaths: string[] = [];
+
+        root.walkDecls((decl: { value: string }) => {
+          const urlMatch = decl.value.match(/url\(['"]?([^'")]+)['"]?\)/);
+          if (urlMatch) {
+            const urlPath = urlMatch[1];
+            filePaths.push(urlPath);
+          }
+        });
+
+        try {
+          const processedFiles = await processor.process(filePaths);
+          processedFiles.forEach((filePath, index) => {
+            const urlPath = filePaths[index];
+
+            root.walkDecls((decl: { value: string; source: any }) => {
+              if (decl.value.includes(urlPath)) {
+                decl.value = decl.value.replace(urlPath, filePath);
+              }
+            });
+          });
+        } catch (error) {
+          result.warn(`Error processing files: ${error.message}`);
+        }
       }
     },
   };
