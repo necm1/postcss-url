74c4bf10f733a0bcab90fbeca2f1ba68cf9cd028
necm1 <necmi.m.hasan@outlook.de>
2024-07-24 18:49:29 +0200

    fix: extended Processor with URLProcessor

--- src/interface/processor.interface.ts
+++ src/interface/processor.interface.ts
@@ -1,13 +1,14 @@
+import { URLProcessorOptions } from './url-processor.interface';
+
 /**
  * Interface for processor options.
  * @interface ProcessorOptions
  */
-export interface ProcessorOptions {
+export interface ProcessorOptions extends URLProcessorOptions {
   url:
     | 'copy'
     | 'custom'
     | 'inline'
     | 'rebase'
     | ((asset: { url: string }) => string);
-  options: any;
 }
